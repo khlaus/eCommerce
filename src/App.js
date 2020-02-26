655730b4fe43f716506f73a23fa0cb4c96767b75
khlaus <khlaus90@yahoo.com>
2020-02-26 19:51:23 +0200

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
           });
         });
       } else {
-        setCurrentUser({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
